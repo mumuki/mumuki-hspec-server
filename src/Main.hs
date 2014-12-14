df22e8652aaa9c99867a3f93e85494b9ab0d1694
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-12-14 19:47:04 -0300

    Implementing test running

--- src/Main.hs
+++ src/Main.hs
@@ -5,20 +5,34 @@ import           Control.Applicative
 import           Snap.Core
 import           Snap.Util.FileServe
 import           Snap.Http.Server
+import           Control.Monad.Trans (liftIO)
+import qualified Data.ByteString.Lazy as L
+import           Data.ByteString.Char8 (pack)
+import           System.Process (readProcessWithExitCode)
+import           System.IO (hClose)
+import           System.Directory (removeFile)
+import           System.IO.Temp (openTempFile)
+import           System.Directory (getTemporaryDirectory)
 
 main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
-site =
-    ifTop (writeBS "hello world") <|>
-    route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
-          ] <|>
-    dir "static" (serveDirectory ".")
+site = route [ ("test", testHandler) ]
 
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
+testHandler :: Snap ()
+testHandler = do
+    content <- readRequestBody 102400
+    result <- liftIO . runTest  $ content
+    writeBS . pack $ result
+
+runTest :: L.ByteString -> IO String
+runTest content = do
+  base <- getTemporaryDirectory
+  (path, fileHandle) <- openTempFile base "compilation"
+  L.hPutStr fileHandle content
+  hClose fileHandle
+  (_, out, err) <- readProcessWithExitCode "runhaskell" [path] ""
+  removeFile path
+  return (out ++ err)
+
