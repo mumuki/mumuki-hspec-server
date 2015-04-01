e281772a312fdb717a4c3eee90d1900aa793013d
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-01 17:19:45 -0300

    Extending inspections parsing

--- src/ExpectationsRunner.hs
+++ src/ExpectationsRunner.hs
@@ -11,8 +11,12 @@ runExpectations es content = map run es
     compileAndEval (P.Expectation binding inspection) = (compile inspection) binding content
 
 compile :: String -> Inspection
-compile "HasLambda" = hasLambda
-compile "HasGuards" = hasGuards
-compile "HasComposition" = hasComposition
-compile "HasBinding" = hasBinding
+compile x           | "Not:" `isInfixOf`      x = not.compile (drop 4 x)
+compile x           | "HasUsage:" `isInfixOf` x = hasUsage (drop 10 x)
+compile "HasLambda"          = hasLambda
+compile "HasGuards"          = hasGuards
+compile "HasComposition"     = hasComposition
+compile "HasBinding"         = hasBinding
+compile "HasDirectRecursion" = hasBinding
+compile "HasComprehension"   = hasBinding
 
--- test/ExpectationsRunnerSpec.hs
+++ test/ExpectationsRunnerSpec.hs
@@ -4,7 +4,7 @@ import           Test.Hspec
 import           ExpectationsRunner (runExpectations)
 import           Protocol
 
-sampleSubmission = "x = g x\n\
+sampleSubmission = "x = m x\n\
                    \y x | x == 1  = z . k $ m\n\
                    \    | otherwise = 0"
 spec :: Spec
@@ -13,10 +13,27 @@ spec = do
     let yHasComposition = Expectation "y" "HasComposition"
     let yHasGuards      = Expectation "y" "HasGuards"
     let xHasGuards      = Expectation "x" "HasGuards"
-    let expectations = [ yHasComposition, yHasGuards, xHasGuards ]
+    let xHasNotGuards   = Expectation "x" "Not:HasGuards"
+    let xHasUsageOfM    = Expectation "x" "HasUsage:m"
+
 
     it "evals expectations" $ do
+      let expectations = [ yHasComposition, yHasGuards, xHasGuards ]
+
       runExpectations expectations sampleSubmission `shouldBe` [
         ExpectationResult yHasComposition True,
         ExpectationResult yHasGuards True,
         ExpectationResult xHasGuards False ]
+
+    it "evals negated expectations" $ do
+      let expectations = [ xHasNotGuards ]
+
+      runExpectations expectations sampleSubmission `shouldBe` [
+        ExpectationResult xHasHotGuards True ]
+
+
+    it "evals parameterized expectations" $ do
+      let expectations = [ xHasUsageOfM ]
+
+      runExpectations expectations sampleSubmission `shouldBe` [
+        ExpectationResult xHasUsageOfM True ]
