e1ff0e739e14a81bae096cd2d81923a939a5a79d
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-05 17:40:57 -0300

    Upgrading hs-inspector

--- src/ExpectationsRunner.hs
+++ src/ExpectationsRunner.hs
@@ -2,14 +2,20 @@ module ExpectationsRunner where
 
 import qualified Protocol as P
 import           Language.Haskell.Inspector
+import           Language.Haskell.Inspector.Smell as S
 import           Data.List (isInfixOf)
 
 runExpectations :: [P.Expectation] ->  String -> [P.ExpectationResult]
-runExpectations es content = map run es
+runExpectations es content = map run es ++ smellsResults
   where
     run e = P.ExpectationResult e (compileAndEval e)
 
     compileAndEval (P.Expectation binding inspection) = (compile inspection) binding content
+
+    smellsResults = filter isFailed . map run $ smellInspections
+
+smellInspections :: [Inspection]
+smellInspections = map negateInspection [hasRedundantIf, hasRedundantLambda, hasRedudantBooleanComparison]
 
 compile :: String -> Inspection
 compile x | "Not:" `isInfixOf`      x = negateInspection $ compile (remove "Not:" x)
@@ -22,7 +28,6 @@ compile "HasDirectRecursion" = hasDirectRecursion
 compile "HasComprehension"   = hasComprehension
 compile "HasIf"              = hasIf
 compile "HasConditional"     = hasConditional
-
 
 remove xs = drop (length xs)
 
