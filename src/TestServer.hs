0c1ee60231fb9522d6cf247415801ecfc406c0b4
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-14 15:27:29 -0300

    Using smells detector in server

--- src/TestServer.hs
+++ src/TestServer.hs
@@ -4,12 +4,15 @@ import qualified Protocol as P
 import qualified TestRunner
 import qualified TestCompiler
 import qualified ExpectationsRunner
+import qualified SmellsDetectorRunner
+
 
 process :: P.Request -> IO P.Response
 process r = do
             (exit, out) <- TestRunner.runTest . compile $ r
             let expectationResults = ExpectationsRunner.runExpectations (P.expectations r)  (P.content r)
-            return $ P.Response exit out expectationResults
+            let smellsResuls = SmellsDetectorRunner.runSmellsDetection (P.content r)
+            return $ P.Response exit out (expectationResults ++ smellsResuls)
 
 compile :: P.Request -> String
 compile request = TestCompiler.compile (P.test request) (P.extra request) (P.content request)
