d140c78f293262080b67ca48ba38758b46bad9e3
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-06 23:47:37 -0300

    Adding JsonFormatter

---
+++ src/JsonFormatter.hs
@@ -0,0 +1,17 @@
+module JsonFormatter where
+
+import Test.Hspec.Formatters
+import Data.List
+
+
+jsonFormatter :: Formatter
+jsonFormatter = silent {
+  exampleSucceeded = \p ->
+    write $ "{\"title\":"++ formatPath p ++",\"status\":\"passed\",\"result\":\"\"},"
+, exampleFailed    = \p (Right e) ->
+    write $ "{\"title\":"++ formatPath p ++",\"status\":\"failed\",\"result\":\""++ e ++"\"},"
+, headerFormatter  = write $ "["
+, footerFormatter  = write $ "{}]"
+}
+
+formatPath (ps, p) = intercalate " " (ps ++ [p])
