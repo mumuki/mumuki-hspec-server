4a2374f9ea3b9dc5d36cf6aed8db9e711114a0a8
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-10 02:42:13 -0300

    Updating local prod config

--- config/test/Config.hs
+++ config/test/Config.hs
@@ -8,5 +8,8 @@ mumukiUrl :: ByteString
 mumukiUrl = "http://mumuki.herokuapp.com"
 
 runhaskellArgs :: [String]
-runhaskellArgs = []
+--Local:
 --runhaskellArgs = ["-package-db=./.cabal-sandbox/x86_64-linux-ghc-7.6.3-packages.conf.d"]
+
+--Travis:
+runhaskellArgs = []
