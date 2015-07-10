49b2d2396e255e7d3e7c65ca644a022545d9a017
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-10 00:56:53 -0300

    Installing in addition to building

--- config/test/Config.hs
+++ config/test/Config.hs
@@ -8,4 +8,5 @@ mumukiUrl :: ByteString
 mumukiUrl = "http://mumuki.herokuapp.com"
 
 runhaskellArgs :: [String]
-runhaskellArgs = ["-package-db=./.cabal-sandbox/x86_64-linux-ghc-7.6.3-packages.conf.d"]
+runhaskellArgs = []
+--runhaskellArgs = ["-package-db=./.cabal-sandbox/x86_64-linux-ghc-7.6.3-packages.conf.d"]
