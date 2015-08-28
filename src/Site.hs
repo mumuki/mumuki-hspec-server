e3a333fb55964d9199f799f147ef520abb3063fd
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-08-28 10:41:52 -0300

    Adding query to routes

--- src/Site.hs
+++ src/Site.hs
@@ -8,16 +8,24 @@ import           Control.Applicative
 import           Snap.Core
 import           Control.Monad.Trans (liftIO)
 import           Server.Test as TestServer
+import           Server.Query as QueryServer
 
 site :: Snap ()
 site = method POST (
-          route [ ("test", testHandler) ]) <|>
+          route [ ("test", testHandler),
+                  ("query", queryHandler) ]) <|>
        method GET  (redirect Config.mumukiUrl)
 
+
 testHandler :: Snap ()
-testHandler = do
+testHandler = handler TestServer.process
+
+queryHandler :: Snap ()
+queryHandler = handler QueryServer.process
+
+
+handler f = do
     setTimeout 8
     Just request <-  decode <$> readRequestBody 102400
-    result  <- liftIO . TestServer.process $ request
+    result  <- liftIO . f $ request
     writeLBS . encode $ result
-
