c9159fdf24f09dc7ebedc9693183904d3b3a6137
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-08-28 03:25:19 -0300

    Adding QueryServer draft

---
+++ src/QueryServer.hs
@@ -0,0 +1,15 @@
+module QueryServer (process) where
+
+import QueryProtocol
+
+process :: Request -> IO Response
+process = fmap toResponse.runQuery.compileRequest
+
+
+compileRequest :: Request -> String
+compileRequest (Request query content extra) = content ++ extra ++ "main = putStr.show $ " ++ query
+
+runQuery :: String -> IO (String, String)
+runQuery compilation = return ("errored", "unimplemented")
+
+toResponse (exit, out) = Response exit out
---
+++ test/QueryRunnerSpec.hs
@@ -0,0 +1,22 @@
+module QueryRunnerSpec (spec) where
+
+import           Test.Hspec
+import           QueryProtocol
+import           QueryServer
+
+processExit = fmap exit.process
+
+spec = describe "QueryServer" $ do
+  let okQuery = "foo 1"
+
+  let okCode        = "foo = bar"
+  let okCodeOnExtra = "foo = (*2)"
+
+  let extraCode     = "bar = (+1)"
+
+  it "should errored on empty request" $ do
+    processExit emptyRequest `shouldReturn` "errored"
+
+  it "should pass on ok request" $ do
+    processExit (emptyRequest { content = okCode, query = okQuery }) `shouldReturn` "passed"
+
